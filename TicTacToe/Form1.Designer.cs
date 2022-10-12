c48003490e0649e08ad9a78655a017c2cfb90fc6
nnastya17 <nnastya1703@gmail.com>
2022-10-12 12:19:36 +0300

    first commit

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(319, 166);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(337, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(400, 400);
+            this.panel2.TabIndex = 2;
+            this.panel2.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(20, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(282, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ким ви хочете грати?";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(49, 43);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(166, 43);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Location = new System.Drawing.Point(12, 184);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(319, 54);
+            this.panel3.TabIndex = 3;
+            this.panel3.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(3, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 29);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Результат";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 433);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "ХО";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        PictureBox[] GamePole = new PictureBox[9];
+        int Player = 0, Computer = 0;
+
+        int[] GamePoleMap = {0,0,0,
+                             0,0,0,
+                             0,0,0 };
+ 
+        string[] ImgName =
+        {
+            "Blank.png",
+            "X.png",
+            "O.png"
+        };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void MainPole()
+        {
+            int DX = 0, DY = 0;
+            int HeighP = 100, 
+                WhidthP = 100,
+                IndexPicture = 0;
+            string NAME = "P_";
+
+            for (int YY = 0; YY < 3; YY++)
+            {
+                for (int XX = 0; XX < 3; XX++)
+                {
+                    GamePole[IndexPicture] = new PictureBox()
+                    {
+                        Name = NAME + IndexPicture,                 
+                        Height = HeighP,                           
+                        Width = WhidthP,                           
+                        Image = Image.FromFile("Blank.png"),        
+                        SizeMode = PictureBoxSizeMode.StretchImage,  
+                        BorderStyle = BorderStyle.FixedSingle,
+                        Location = new Point(DX, DY)
+                    };
+
+                    GamePole[IndexPicture].Click += Picture_Click;
+
+                    panel2.Controls.Add(GamePole[IndexPicture]);  
+                    IndexPicture++;
+
+                    DX += WhidthP;  
+                }
+                DY += HeighP; 
+                DX = 0; 
+            }
+        }
+        private void Picture_Click(object sender, EventArgs e)
+        {
+            if (CanStap())
+            {
+                PictureBox ClickImage = sender as PictureBox;
+                string[] ParsName = ClickImage.Name.Split('_');
+
+                int IndexSelectImage = Convert.ToInt32(ParsName[1]);
+
+                GamePole[IndexSelectImage].Image = Image.FromFile(ImgName[Player]);
+                GamePoleMap[IndexSelectImage] = Player;
+
+                if (!TestWin(Player))
+                {
+                    LoockPole();
+                    PC_Step();
+                    UnLoockPole();
+                }
+                else
+                {
+                    panel3.Visible = true;
+                    label3.Text = "Ви виграли!";
+                    LoockPole();
+                }
+            }
+        }
+        void LoockPole()
+        {
+            foreach (PictureBox P in GamePole) P.Enabled = false;
+        }
+        void UnLoockPole()
+        {
+            int Indexx = 0;
+            foreach (PictureBox P in GamePole)
+            {
+                if (GamePoleMap[Indexx++] == 0) P.Enabled = true;
+            }
+        }
+        bool CanStap()
+        {
+            foreach (int s in GamePoleMap) 
+                if (s == 0) return true;
+ 
+            if (TestWin(Player))
+            {
+                label3.Text = "Ви виграли";
+                LoockPole();
+                panel3.Visible = true; 
+                return false;
+            } 
+            if (TestWin(Computer))
+            {
+                label3.Text = "Ви програли";
+                panel3.Visible = true; 
+                LoockPole();
+                panel3.Visible = false;
+                return false;
+            }
+             
+            label3.Text = "Нічия"; 
+
+            panel3.Visible = true;
+            LoockPole();
+
+            return false;
+        }
+        bool TestWin(int WHO)
+        { 
+            int[,] WinVariant =
+            {      {    
+                    1,1,1,   
+                    0,0,0,  
+                    0,0,0   
+                }, {    
+                    0,0,0,  
+                    1,1,1,  
+                    0,0,0   
+                }, {     
+                    0,0,0,  
+                    0,0,0,  
+                    1,1,1   
+                }, {    
+                    1,0,0,  
+                    1,0,0,  
+                    1,0,0    
+                }, {    
+                    0,1,0,  
+                    0,1,0,   
+                    0,1,0    
+                }, {     
+                    0,0,1,  
+                    0,0,1,  
+                    0,0,1   
+                }, {     
+                    1,0,0,   
+                    0,1,0,   
+                    0,0,1    
+                }, {     
+                    0,0,1,    
+                    0,1,0,    
+                    1,0,0    
+                }
+            };
+             
+            int[] TestMap = new int[GamePoleMap.Length];
+            for (int I = 0; I < GamePoleMap.Length; I++) 
+                if (GamePoleMap[I] == WHO) TestMap[I] = 1;
+             
+            for (int Variant_Index = 0; Variant_Index < WinVariant.GetLength(0); Variant_Index++)
+            { 
+                int WinState = 0;
+                for (int TestIndex = 0; TestIndex < TestMap.Length; TestIndex++)
+                { 
+                    if (WinVariant[Variant_Index, TestIndex] == 1)
+                    { 
+                        if (WinVariant[Variant_Index, TestIndex] == TestMap[TestIndex]) WinState++;
+                    } 
+                    if (WinState == 3) return true;
+                } 
+            } 
+            return false; 
+        }
+        void PC_Step()
+        { 
+            Random Rand = new Random();
+        GENER: 
+            if (CanStap())
+            { 
+                int IndexStep = Rand.Next(0, 8); 
+                if (GamePoleMap[IndexStep] == 0)
+                { 
+                    GamePole[IndexStep].Image = Image.FromFile(ImgName[Computer]); 
+                    GamePoleMap[IndexStep] = Computer; 
+                }
+                else goto GENER;
+                if (TestWin(Computer))
+                {
+                    panel3.Visible = true;
+                    label3.Text = "Ви Програли";
+                }
+            }
+        }
+         
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MainPole();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Player = 2;
+            Computer = 1;
+            panel1.Visible = false;
+            panel2.Visible = true;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Player = 1;
+            Computer = 2;
+            panel1.Visible = false;
+            panel2.Visible = true;
+        }
+    }
+}
